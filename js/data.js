eb02450c8fa4b28dc767148d7ec03b4a81812e6f
Bert Belder <bertbelder@gmail.com>
2012-02-19 20:55:18 +0000

    "Correct XML export order"

--- js/data.js
+++ js/data.js
@@ -1,21 +1,21 @@
 $(function() {
+  window.agents = new AgentList();
   window.roles = new RoleList();
   window.institutions = new InstitutionList();
-  window.agents = new AgentList();
   window.components = new ComponentList();
   window.actionSituations = new ActionSituationList();
   window.actions = new ActionList();
   window.roleEnactments = new RoleEnactmentList();
-  window.domainProblemVariables = new DomainProblemVariableList();
   window.validationVariables = new ValidationVariableList();
+  window.domainProblemVariables = new DomainProblemVariableList();
 
+  agents.fetch();
   roles.fetch();
   institutions.fetch();
-  agents.fetch();
   components.fetch();
   actionSituations.fetch();
   actions.fetch();
   roleEnactments.fetch();
-  domainProblemVariables.fetch();
   validationVariables.fetch();
+  domainProblemVariables.fetch();
 });
