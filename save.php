98af68fa0ccd5c169ee715272afeb5e45081293d
hbelder <hbelder@3d6e4fad-627e-4bc8-adb0-66572415ef75>
2012-05-23 21:27:31 +0000

    Hello 2012. Reverse magic_quotes_gpc
    OMG!!!1
    
    git-svn-id: https://svn.eeni.tbm.tudelft.nl/AminehGhorbani/MAIAwebTool/maiaOnline@1757 3d6e4fad-627e-4bc8-adb0-66572415ef75

--- save.php
+++ save.php
@@ -1,5 +1,23 @@
 <?php
 
+// This is why I hate PHP.
+if (get_magic_quotes_gpc()) {
+  $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+  while (list($key, $val) = each($process)) {
+    foreach ($val as $k => $v) {
+      unset($process[$key][$k]);
+      if (is_array($v)) {
+        $process[$key][stripslashes($k)] = $v;
+        $process[] = &$process[$key][stripslashes($k)];
+      } else {
+        $process[$key][stripslashes($k)] = stripslashes($v);
+      }
+    }
+  }
+  unset($process);
+}
+
+
 $description = $_POST['description'];
 $email = $_POST['email'];
 $data = $_POST['data'];
